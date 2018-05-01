d8828f44551d6b267a08e0ddd356f6919efc2aa7
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-01 14:07:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const increaseRankBy = (n) => {
   let rankedList = document.querySelectorAll('ul.ranked-list li')
   for(let i = 0; i>rankedList.length; i++){
    rankedList[i].innerHTML = (parseInt(rankedList[i].innerHTML) + n).toString()
-   console.log(rankedList[i])
   }
 };
 
