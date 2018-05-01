de92f2bac321def37f054caa386b1e2d6a815ab9
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-01 14:01:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+const getFirstSelector = (selector) => {
+  return document.querySelector(selector);
+};
+
+const nestedTarget = () => {
+  return document.querySelector('#nested .target');
+};
+
+const increaseRankBy = (n) => {
+  let rankedList = document.getElementsByClassName('ranked-list').querySelectorAll('ul.ranked-list li')
+  console.log(rankedList)
+  for(let i = 0; i>rankedList.length; i++){
+   rankedList[i].innerHTML = (parseInt(rankedList[i].innerHTML) + n).toString()
+  }
+};
+
+const deepestChild = () =>{
+  
+}
